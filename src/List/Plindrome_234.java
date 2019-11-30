c0ad30cc743ab25ee96d9fe7d8e77ee196f10426
Joey-957 <qiaoqi2014@outlook.com>
2019-11-30 21:23:37 +0800

    tips.md

--- src/List/Plindrome_234.java
+++ src/List/Plindrome_234.java
@@ -2,6 +2,7 @@ package List;
 
 /**
  * 请判断一个链表是否为回文链表。
+ * error:理解成了[1,2,3,4,1,2,3],其实应该是[1,2,3,4,3,2,1]
  */
 public class Plindrome_234 {
     //头插法建立链表
@@ -33,9 +34,71 @@ public class Plindrome_234 {
             head = head.next;
         }
     }
+
     //---------------------------------------------------------------
+
+    /**
+     * 版本一：理解出错了
+     * @param head
+     * @return
+     */
     public boolean isPalindrome(ListNode head) {
+        if (head==null){
+            return false;
+        }
+        //遍历链表，得到链表结点个数
+        int amount = 0;
+        ListNode flag = head;
+        while (flag != null) {
+            amount++;
+            flag = flag.next;
+        }
+        if  (amount==1){
+            return true;
+        }
+        if (amount==2){
+            if(head.val==head.next.val){
+                return true;
+            }else{
+                return false;
+            }
+        }
+        int temp = amount / 2 + 1;
+        flag = head;
+        for (int k = 0; k < temp; k++) {
+            flag = flag.next;
+        }
+        while (flag != null) {
+            if (flag.val == head.val) {
+                flag = flag.next;
+                head = head.next;
+            } else {
+                return false;
+            }
+        }
         return true;
+    }
+
+    /**
+     * 版本二：通过使用快慢指针，不用知道节点的个数节能得到中点指针
+     * @param head
+     * @return
+     */
+    public boolean isPalindrome_2(ListNode head) {
+        if (head==null||head.next==null){
+            return true;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] argus){
+        Plindrome_234 plindrome_234 = new Plindrome_234();
+        ListNode head = new ListNode(1);
+        plindrome_234.headInsertion(head,2);
+        plindrome_234.headInsertion(head,2);
+        Boolean t = plindrome_234.isPalindrome(head);
+        System.out.println(t);
     }
 
 }
